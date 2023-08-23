6327b2b342efe35da50483462a2283d1b00d0155
andersondomingues <ti.andersondomingues@gmail.com>
2023-08-23 16:31:03 -0300

    add parameter parsing to porca

--- porca/src/main.cpp
+++ porca/src/main.cpp
@@ -1,40 +1,29 @@
 #include <iostream>
-
+#include <map>
 #include <vector>
+
 #include "../include/main.h"
 #include "../include/tty.h"
 
-std::string_view get_option(
-    const std::vector<std::string_view>& args, 
-    const std::string_view& option_name) {
-    for (auto it = args.begin(), end = args.end(); it != end; ++it) {
-        if (*it == option_name)
-            if (it + 1 != end)
-                return *(it + 1);
-    }
-    
-    return "";
-}
-
-
-
 int main(int argc, char** argv){
   
-  //prints the blue whale 
+  std::map<std::string, std::string> params;
+
+  // prints the orca whale logo and welcome message 
   for(const auto& s: WELCOME_MESSAGE)
     std::cout << (tty::white + tty::b_blue + tty::a_bold) << s << tty::a_reset << std::endl;
 
+  // parse parameters values into a dictionary
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+      if (arg.substr(0, 1) == "-") {
+          std::string key = arg.substr(1);
+          std::string value = (i + 1 < argc && argv[i + 1][0] != '-') ? argv[i + 1] : "";
+          params[key] = value;
+      }
+  }
 
-  // add command line options to a string view vector
-  const auto options = std::vector<std::string_view>(argv + 1, argv + argc);
-
-  // // list all options
-  // for (const auto opt: options){
-  //   if (opt[0] == '-') {
-  //     std::cout << tty::b_blue <<  tty::blue << "text" << tty::a_reset << std::endl;
-  //   }
-  // }
-
-
+  for (const auto& kvp : params)
+    std::cout << "Key: " << kvp.first << ", Value: " << kvp.second << std::endl;
 
 }
